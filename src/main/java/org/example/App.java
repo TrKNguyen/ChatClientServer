dc66abd688d85c88b017124113633d82f653926e
TrKNguyen <nguyenusaco@gmail.com>
2023-02-15 15:53:50 +0700

    Add to Github

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
-package org.example;
+//package org.example;
 
 /**
  * Hello world!
  *
  */
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        System.out.println(n*100);
     }
 }
